c64f381d5d84d25c294a118a2b5e56b76b33f0f8
Shane Parker <shane.tim.parker@ea.com>
2021-12-23 13:57:52 -0500

    Added support for demoting IMMUNE to RESIST

--- scripts/manager_action_damage_bi.lua
+++ scripts/manager_action_damage_bi.lua
@@ -6,9 +6,9 @@
 local getReductionTypeOriginal;
 local checkReductionTypeHelperOriginal;
 local getDamageAdjustOriginal;
-local messageDamageOriginal;
 
-local aResistanceMessages;
+local sResistanceMessage;
+local tReductions = {};
 
 function onInit()
 	getReductionTypeOriginal = ActionDamage.getReductionType;
@@ -19,52 +19,119 @@ function onInit()
 	
 	getDamageAdjustOriginal = ActionDamage.getDamageAdjust;
 	ActionDamage.getDamageAdjust = getDamageAdjust;
-	
-	messageDamageOriginal = ActionDamage.messageDamage;
-	ActionDamage.messageDamage = messageDamage;
 end
 
 function getReductionType(rSource, rTarget, sEffectType)
 	local aFinal = getReductionTypeOriginal(rSource, rTarget, sEffectType);
 
+	local bIgnoreAll = false;
 	local aEffects = EffectManager5E.getEffectsByType(rSource, "IGNORE" .. sEffectType, {}, rTarget);
 	for _,rEffect in pairs(aEffects) do
 		for _,sType in pairs(rEffect.remainder) do
-			local rReduction = aFinal[sType];
-			if rReduction then
-				if not rReduction.aIgnored then
-					rReduction.aIgnored = {};
+			if sType == "all" then
+				for _,sDamage in ipairs(DataCommon.dmgtypes) do
+					addIgnoredDamageType(aFinal, sDamage, sEffectType);
 				end
-				table.insert(rReduction.aIgnored, sType);
+				bIgnoreAll = true;
+				break;
 			end
+			addIgnoredDamageType(aFinal, sType, sEffectType);
+		end
+		if bIgnoreAll then
+			break;
+		end
+	end
+	
+	if sEffectType == "IMMUNE" and aFinal["all"] then
+		local rReduction = aFinal["all"];
+		aFinal["all"] = nil;
+		for _,sDamage in ipairs(DataCommon.dmgtypes) do
+			aFinal[sDamage] = rReduction;
+		end
+	end
 
-			rReduction = aFinal["all"];
-			if rReduction then
-				if not rReduction.aIgnored then
-					rReduction.aIgnored = {};
-				end
-				table.insert(rReduction.aIgnored, sType);
-				if sEffectType == "IMMUNE" then
-					aFinal["all"] = nil;
+	if sEffectType == "RESIST" then -- Represents the last set
+		bDemoteAll = false;
+		aEffects = EffectManager5E.getEffectsByType(rSource, "DEMOTEIMMUNE", {}, rTarget);
+		for _,rEffect in pairs(aEffects) do
+			for _,sType in pairs(rEffect.remainder) do
+				if sType == "all" then
 					for _,sDamage in ipairs(DataCommon.dmgtypes) do
-						aFinal[sDamage] = rReduction;
+						addDemotedDamagedType(aFinal, tReductions["IMMUNE"], sDamage);
 					end
+					bDemoteAll = true;
+					break;
 				end
+				addDemotedDamagedType(aFinal, tReductions["IMMUNE"], sType);
+			end
+			if bDemoteAll then
+				break;
 			end
 		end
 	end
 
+	tReductions[sEffectType] = aFinal;
 	return aFinal;
+end
+
+function addIgnoredDamageType(aEffects, sDamageType, sEffectType)
+	local rReduction = aEffects[sDamageType];
+	if rReduction then
+		if not rReduction.aIgnored then
+			rReduction.aIgnored = {};
+		end
+		table.insert(rReduction.aIgnored, sDamageType);
+	end
+
+	rReduction = aEffects["all"];
+	if rReduction then
+		if not rReduction.aIgnored then
+			rReduction.aIgnored = {};
+		end
+		table.insert(rReduction.aIgnored, sDamageType);
+	end
+end
+
+function addDemotedDamagedType(aResistEffects, aImmuneEffects, sDamageType)
+	local rReduction = aImmuneEffects[sDamageType];
+	if rReduction and not (rReduction.aIgnored and StringManager.contains(rReduction.aIgnored, sDamageType)) then
+		aResistEffects[sDamageType] = UtilityManager.copyDeep(rReduction);
+
+		if not rReduction.aDemoted then
+			rReduction.aDemoted = {};
+		end
+		table.insert(rReduction.aDemoted, sDamageType);
+	end
+
+	rReduction = aImmuneEffects["all"];
+	if rReduction then
+		aResistEffects[sDamageType] = UtilityManager.copyDeep(rReduction);
+	
+		if not rReduction.aDemoted then
+			rReduction.aDemoted = {};
+		end
+		table.insert(rReduction.aDemoted, sDamageType);
+	end
 end
 
 function checkReductionTypeHelper(rMatch, aDmgType)
 	local result = checkReductionTypeHelperOriginal(rMatch, aDmgType);
-	if result and rMatch.aIgnored and #rMatch.aIgnored > 0 then
-		local bMatchIgnore = false;
-		for _,sIgnored in pairs(rMatch.aIgnored) do
-			if StringManager.contains(aDmgType, sIgnored) then
-				table.insert(aResistanceMessages, "[RESISTANCE IGNORED]")
-				return false;
+	if result then
+		if rMatch.aIgnored then
+			for _,sIgnored in pairs(rMatch.aIgnored) do
+				if StringManager.contains(aDmgType, sIgnored) then
+					sResistanceMessage ="[RESISTANCE IGNORED]";
+					return false;
+				end
+			end
+		end
+
+		if rMatch.aDemoted then
+			for _,sDemoted in pairs(rMatch.aDemoted) do
+				if StringManager.contains(aDmgType, sDemoted) then
+					sResistanceMessage ="[IMMUNITY DEMOTED]";
+					return false;
+				end
 			end
 		end
 	end
@@ -74,10 +141,9 @@ end
 function checkNumericalReductionTypeHelper(rMatch, aDmgType, nLimit)
 	local result = checkNumericalReductionTypeHelperOriginal(rMatch, aDmgType, nLimit);
 	if (result ~= 0) and rMatch.aIgnored and #rMatch.aIgnored > 0 then
-		local bMatchIgnore = false;
 		for _,sIgnored in pairs(rMatch.aIgnored) do
 			if StringManager.contains(aDmgType, sIgnored) then
-				table.insert(aResistanceMessages, "[RESISTANCE IGNORED]")
+				sResistanceMessage ="[RESISTANCE IGNORED]";
 				return 0;
 			end
 		end
@@ -86,15 +152,17 @@ function checkNumericalReductionTypeHelper(rMatch, aDmgType, nLimit)
 end
 
 function getDamageAdjust(rSource, rTarget, nDamage, rDamageOutput)
-	aResistanceMessages = rDamageOutput.tNotifications;
+	tReductions = {
+		["VULN"] = {},
+		["RESIST"] = {},
+		["IMMUNE"] = {}
+	};
 	local nDamageAdjust, bVulnerable, bResist = getDamageAdjustOriginal(rSource, rTarget, rDamageOutput.nVal, rDamageOutput);
 
-	return nDamageAdjust, bVulnerable, bResist;
-end
+	if sResistanceMessage then
+		table.insert(rDamageOutput.tNotifications, sResistanceMessage);
+		sResistanceMessage = nil;
+	end
 
-function messageDamage(rSource, rTarget, bSecret, sDamageType, sDamageDesc, sTotal, sExtraResult)
-	--todo probably not needed
-	--table.insert(aResistanceMessages, 1, sExtraResult);
-	--sExtraResult = table.concat(aResistanceMessages, " ");
-	messageDamageOriginal(rSource, rTarget, bSecret, sDamageType, sDamageDesc, sTotal, sExtraResult);
+	return nDamageAdjust, bVulnerable, bResist;
 end
